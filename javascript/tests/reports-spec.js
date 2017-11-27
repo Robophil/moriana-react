3d5020dc6dcfd29e294d01bf5a956a5987fa4aa2
Kevin Doran <kev.doran@gmail.com>
2017-11-27 16:17:10 -0600

    * added AMC to consumption report

--- javascript/tests/reports-spec.js
+++ javascript/tests/reports-spec.js
@@ -1,76 +1,26 @@
-export default {}
+import chai from 'chai'
+const expect = chai.expect
+import {consumptionReport} from 'consumption-report'
 
-// import Moment from 'moment'
-// import reportsReducer, { defaultReportsState, receivedAllAction, runReportAction } from 'alldocs'
-// import { shipmentsFixtures, testExpiration } from 'report-fixtures'
-//
-// import chai from 'chai'
-// const expect = chai.expect
-//
-// const defaultFiltersState = reportsReducer(defaultReportsState, receivedAllAction(shipmentsFixtures, 'test warehouse', {}))
-// const lastMonthName = Moment.utc().subtract(1, 'months').startOf('month').format('MMMM YYYY')
-//
-// const consumptionState = reportsReducer(defaultFiltersState, runReportAction('consumption'))
-// const consumptionStateItemLevel = reportsReducer(defaultFiltersState, runReportAction('consumption', 'batches', 1))
-// const consumptionStateCategoryFilter = reportsReducer(defaultFiltersState, runReportAction('consumption', 'categories', 1))
-//
-// export default {
-//   'Report setup': {
-//     'should return a list of available date filters' () {
-//       expect(defaultFiltersState.allDateFilters[0].name).eq(lastMonthName)
-//     },
-//     'should return a list of available category filters' () {
-//       expect(defaultFiltersState.allCategoryFilters[0].name).eq('All Categories')
-//       expect(defaultFiltersState.allCategoryFilters[1].name).eq('catA')
-//     },
-//     'should return a list of available batch filters' () {
-//       // expect(defaultFiltersState.allBatchFilters[0].itemLevel).eq(false)
-//       // expect(defaultFiltersState.allBatchFilters[1].itemLevel).eq(true)
-//     },
-//     'should default set filters to their first available filter option' () {
-//       expect(defaultFiltersState.categoryFilter.name).eq('All Categories')
-//       expect(defaultFiltersState.dateFilter.name).eq(lastMonthName)
-//       expect(defaultFiltersState.batchFilter.itemLevel).eq(false)
-//     },
-//     'should return a hash of all available items' () {
-//       expect(defaultFiltersState.allItemsMap['abc__catA']).not.eq(undefined)
-//       expect(defaultFiltersState.allItemsMap['abc__catA'][`${testExpiration}__null`][0].quantity).eq(1)
-//       expect(defaultFiltersState.allItemsMap['bcd__catA'][`null__abc`].length).eq(2)
-//     }
-//   },
-//   'Running a report': {
-//     'should return report rows and report headers' () {
-//       expect(consumptionState.displayRows instanceof Array).eq(true)
-//       expect(consumptionState.displayHeaders instanceof Array).eq(true)
-//     },
-//     'should default to batch level' () {
-//       expect(consumptionState.displayRows.length).gt(5)
-//     },
-//     'should filter out batch rows with all zeros but leave one row at the item level' () {
-//       expect(consumptionState.displayRows.length).eq(6)
-//     },
-//     'should order alphabetically' () {
-//       const expectedOrder = ['abc', 'bcd', 'bcd', 'cde', 'def', 'efg']
-//       consumptionState.displayRows.forEach((row, i) => {
-//         expect(row.item).eq(expectedOrder[i])
-//       })
-//     }
-//   },
-//   'Running a report with item level filter': {
-//     'should return rows condensed to item level' () {
-//       expect(consumptionStateItemLevel.displayRows.length).eq(5)
-//       expect(consumptionStateItemLevel.displayRows[0].expiration).eq(undefined)
-//       expect(consumptionStateItemLevel.displayRows[1].opening).eq(3)
-//     }
-//   },
-//   'Running a report with category filters': {
-//     'should only return rows for the specified category' () {
-//       expect(consumptionStateCategoryFilter.displayRows.length).eq(4)
-//       expect(consumptionStateCategoryFilter.displayRows[0].category).eq('catA')
-//       const catBState = reportsReducer(defaultFiltersState, runReportAction('consumption', 'categories', 2))
-//       expect(catBState.displayRows.length).eq(1)
-//       expect(catBState.displayRows[0].category).eq('catB')
-//     }
-//   }
-//
-// }
+const byItem = [
+  { item: 'test item', category: 'test category', transactions: [
+    { quantity: -15, date: new Date('2015-01-15').toISOString() },
+    { quantity: -15, date: new Date('2015-06-15').toISOString() },
+    { quantity: -15, date: new Date('2015-07-15').toISOString() },
+    { quantity: -15, date: new Date('2015-08-15').toISOString() },
+    { quantity: -15, date: new Date('2015-09-15').toISOString() },
+    { quantity: -15, date: new Date('2015-10-15').toISOString() },
+  ] }
+]
+
+const ALL_TEST_ITEMS = { byItem }
+
+export default {
+  'Consumption report': {
+    'should return rows with AMC details for the last six months' () {
+      const dateRange = { startDate: '2015-10-01', endDate: '2015-11-15' }
+      const {displayRows} = consumptionReport(ALL_TEST_ITEMS, false, dateRange)
+      expect(displayRows[0].amc).eq(13)
+    }
+  }
+}
--- javascript/utils/consumption-report.js
+++ javascript/utils/consumption-report.js
@@ -14,14 +14,18 @@ const DISPLAY_HEADERS = [
   { key: 'negativeAdjustments', name: 'Negative Adjustments' }
 ]
 
-export const consumptionReport = (allItems, atBatchLevel, dateRange, categoryFilter, excludedLocations) => {
+export const consumptionReport = (allItems, atBatchLevel, dateRange, categoryFilter, excludedLocations = {}) => {
   let displayRows = []
-  const {startDate, endDate} = dateRange || {}
+
+  const { startDate, endDate } = dateRange || {}
   const { byItem, byBatch } = allItems
+
   const itemsOrBatches = atBatchLevel ? byBatch : byItem
   const itemsSet = new Set()
+
   itemsOrBatches.forEach(itemOrBatch => {
     const row = getBlankRow(itemOrBatch)
+    row.amc = getAMCByMonths(itemOrBatch.transactions, 6, endDate)
     itemsSet.add(row.item)
     itemOrBatch.transactions.forEach(t => addConsumptionData(row, t, startDate, endDate, excludedLocations))
     if (notAllZeros(row)) {
@@ -29,13 +33,15 @@ export const consumptionReport = (allItems, atBatchLevel, dateRange, categoryFil
       itemsSet.delete(row.item)
     }
   })
-  itemsSet.forEach(item => {
-    displayRows.push(getBlankRow({item}))
-  })
+
+  itemsSet.forEach(item => { displayRows.push(getBlankRow({item})) })
+
   if (categoryFilter) {
     displayRows = displayRows.filter(row => (row.category === categoryFilter))
   }
+
   sortByItemName(displayRows)
+
   return { displayRows, displayHeaders: getItemHeaders(atBatchLevel).concat(DISPLAY_HEADERS) }
 }
 
@@ -46,45 +52,48 @@ const getBlankRow = ({ item, category, expiration, lot }) => {
   }, {item, category, expiration, lot})
 }
 
-export const consumptionReportOld = (allItemsMap, atBatchLevel, dateRange, categoryFilter, excludedLocations) => {
-  let displayRows = []
-  const {startDate, endDate} = dateRange || {}
-  if (!atBatchLevel) {
-    Object.keys(allItemsMap).forEach(itemKey => {
-      const row = getBlankConsumptionRow(false, itemKey)
-      Object.keys(allItemsMap[itemKey]).forEach(batchKey => {
-        allItemsMap[itemKey][batchKey].forEach(t => addTransactionsConsumption(row, t, startDate, endDate, excludedLocations))
-      })
-      displayRows.push(row)
-    })
-  } else {
-    Object.keys(allItemsMap).forEach(itemKey => {
-      const itemsBatchKeys = Object.keys(allItemsMap[itemKey])
-      let row
-      let allBatchesAreZeros = true
-      for (let i = 0; i < itemsBatchKeys.length; i++) {
-        row = getBlankConsumptionRow(true, itemKey, itemsBatchKeys[i])
-        allItemsMap[itemKey][itemsBatchKeys[i]].forEach(t => {
-          addTransactionsConsumption(row, t, startDate, endDate, excludedLocations)
-        })
-        if (!allColumnsAreZeros(row)) {
-          displayRows.push(row)
-          allBatchesAreZeros = false
-        }
-      }
-      if (allBatchesAreZeros) {
-        delete row.expiration
-        delete row.lot
-        displayRows.push(row)
-      }
-    })
-  }
-  sortByItemName(displayRows)
-  if (categoryFilter) {
-    displayRows = displayRows.filter(row => (row.category === categoryFilter))
-  }
-  return { displayRows, DISPLAY_HEADERS: getItemHeaders(atBatchLevel).concat(DISPLAY_HEADERS) }
-}
+// export const consumptionReportOld = (allItemsMap, atBatchLevel, dateRange, categoryFilter, excludedLocations) => {
+//   let displayRows = []
+//   const {startDate, endDate} = dateRange || {}
+//   if (!atBatchLevel) {
+//     Object.keys(allItemsMap).forEach(itemKey => {
+//       const row = getBlankConsumptionRow(false, itemKey)
+//       Object.keys(allItemsMap[itemKey]).forEach(batchKey => {
+//         allItemsMap[itemKey][batchKey].forEach(t => addTransactionsConsumption(row, t, startDate, endDate, excludedLocations))
+//       })
+//       displayRows.push(row)
+//     })
+//   } else {
+//     Object.keys(allItemsMap).forEach(itemKey => {
+//       const itemsBatchKeys = Object.keys(allItemsMap[itemKey])
+//       let row
+//       let allBatchesAreZeros = true
+//       for (let i = 0; i < itemsBatchKeys.length; i++) {
+//         row = getBlankConsumptionRow(true, itemKey, itemsBatchKeys[i])
+//         allItemsMap[itemKey][itemsBatchKeys[i]].forEach(t => {
+//           addTransactionsConsumption(row, t, startDate, endDate, excludedLocations)
+//         })
+//         if (!allColumnsAreZeros(row)) {
+//           displayRows.push(row)
+//           allBatchesAreZeros = false
+//         }
+//       }
+//       if (allBatchesAreZeros) {
+//         delete row.expiration
+//         delete row.lot
+//         displayRows.push(row)
+//       }
+//     })
+//   }
+//
+//   sortByItemName(displayRows)
+//
+//   if (categoryFilter) {
+//     displayRows = displayRows.filter(row => (row.category === categoryFilter))
+//   }
+//
+//   return { displayRows, DISPLAY_HEADERS: getItemHeaders(atBatchLevel).concat(DISPLAY_HEADERS) }
+// }
 
 const getBlankConsumptionRow = (atBatchLevel, itemKey, batchKey) => {
   const row = getItemFromkey(itemKey)
